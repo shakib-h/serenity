6d54ce1cbf4abafb0464accf4f88fce0af2d85c5
Arnaim <146977511+Arnaim@users.noreply.github.com>
2023-10-20 22:07:42 +0600

    New questions now load, the issue has been fixed

--- stressTestWindow.py
+++ stressTestWindow.py
@@ -9,23 +9,24 @@ stressTest.configure(bg="#ffffff")
 questions = [
     '''Q1 : How often do you feel overwhelmed with your life?''',
     '''Q2 : Do you struggle to fall asleep at night?''',
-    '''Q3 : Do you turn to unhealthy food indulgences such as eating junk food, drinking excessively, or eating sugary foods when feeling overwhelmed?''',
+    '''Q3 : Do you turn to unhealthy food indulgences such 
+        as eating junk food, drinking excessively, or eating 
+        sugary foods when feeling overwhelmed?''',
     '''Q4 : Do you experience headaches or muscle tension?''',
     '''Q5 : During work hours, do you have a hard time staying focused and concentrating on the task-at-hand? '''
 ]
-answer_choice = [
-    ["Never"],
-    ["Sometimes"],
-    ["Often"],
-    ["Almost_Always"],
-]
 
-print("hello")
+answer_choice = [
+    ["Never", "Rarely", "Sometimes", "Always"],
+    ["Never", "Rarely", "Sometimes", "Always"],
+    ["Never", "Rarely", "Sometimes", "Always"],
+    ["Never", "Rarely", "Sometimes", "Always"],
+    ["Never", "Rarely", "Sometimes", "Always"]
+]
 
 user_input = []
 ques = 0
 
-# Declare question_text and option_buttons as global variables
 question_text = None
 option_buttons = []
 
@@ -42,17 +43,16 @@ def selected():
 
 def load_question():
     global ques
-    question_text.set(questions[ques])   
+    question_text.set(questions[ques])
     for i in range(4):
-        option_buttons[i].config(text=answer_choice[ques][i])
-    #ques_label.config(text=f"Question {ques+1} of {len(questions)}")
+        option_buttons[i].config(text=answer_choice[ques][i], state=NORMAL) # [MODIFIED] 'state=NORMAL' added [cause of non-error code now]
 
-def stress_level():
+def stress_level(): # [MODIFIED] numbers got modified 
     stress_mapping = {
-        '1': 25,
-        '2': 50,
-        '3': 75,
-        '4': 100
+        '0': 25,
+        '1': 50,
+        '2': 75,
+        '3': 100
     }
     weights = [20, 20, 15, 15, 30]
 
@@ -60,7 +60,7 @@ def stress_level():
     stress_level = 0  
 
     for i, response in enumerate(user_input):
-        stress_level += stress_mapping[str(response + 1)] * (weights[i] / 100)
+        stress_level += stress_mapping[str(response)] * (weights[i] / 100)
 
     stress_result += str(stress_level)
     stress = tk.Label(stressTest, text=stress_result, bg='#ffffff', fg='#111111', font=('Arial', 21))
@@ -83,23 +83,16 @@ def start():
 
     global ques_label
 
-    #for i in range(5):
-    question = tk.Label(stressTest, text=questions[0], bg='#ffffff', fg='#111111', font=('Arial', 21))
-    question.pack()
+    ques_label = tk.Label(stressTest, textvariable=question_text, bg='#ffffff', fg='#111111', font=('Arial', 21))
+    ques_label.pack()
 
-    option1 = tk.Radiobutton(stressTest, text=answer_choice[0], bg='#ffffff', fg='#111111', font=('Arial', 16), value=0, variable=radiovar)
-    option1.pack()
+    # [MODIFIED] updated whole platform
+    for i in range(4):
+        option = tk.Radiobutton(stressTest, text="", bg='#ffffff', fg='#111111', font=('Arial', 16), value=i, variable=radiovar, state=DISABLED)
+        option.pack()
+        option_buttons.append(option)
 
-    option2 = tk.Radiobutton(stressTest, text=answer_choice[1], bg='#ffffff', fg='#111111', font=('Arial', 16), value=1, variable=radiovar)
-    option2.pack()
-
-    option3 = tk.Radiobutton(stressTest, text=answer_choice[2], bg='#ffffff', fg='#111111', font=('Arial', 16), value=2, variable=radiovar)
-    option3.pack()
-
-    option4 = tk.Radiobutton(stressTest, text=answer_choice[3], bg='#ffffff', fg='#111111', font=('Arial', 16), value=3, variable=radiovar)
-    option4.pack()
-
-    option_buttons = [option1, option2, option3, option4]
+    load_question()  # [MODIFIED] Added this line to load the first question
 
     next_button = tk.Button(stressTest, text="Next", command=selected, bg='#ffffff', fg='#111111', font=('Arial', 20))
     next_button.pack()
