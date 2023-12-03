92f4269c5af10e8f17498f4c7d95148888040a45
shakib-h <hello@shakib.com.bd>
2023-12-04 02:13:02 +0600

    final

--- frames/articles.py
+++ frames/articles.py
@@ -6,103 +6,103 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-blogs = [
-     {"title": "Do I Even Need Counseling?", "link": "https://www.betterhelp.com/advice/counseling/do-i-even-need-counseling-the-unique-expansiveness-of-psychotherapy/", "image_url": "https://dz9yg0snnohlc.cloudfront.net/advice/images/betterhelp/7563/43cc4eaab49e5d5f0bf9c8752ed9d9f7-man-stressed-headphones-white-shirt_l_sm.jpg"},
-    {"title": "Is It Time To Start Therapy?", "link": "https://www.betterhelp.com/advice/therapy/is-it-time-to-start-therapy-why-you-shouldnt-procrastinate-on-mental-health/", "image_url": "https://dz9yg0snnohlc.cloudfront.net/advice/images/betterhelp/7564/3888fff2403ef88711369f831050b77f-man-in-yellow-shirt-stressed-on-couch_l_sm.jpg"},
-    {"title": "Is It Okay To Pause Therapy?", "link": "https://www.betterhelp.com/advice/therapy/is-it-okay-to-pause-therapy-understanding-the-risks-and-benefits/", "image_url": "https://dz9yg0snnohlc.cloudfront.net/advice/images/betterhelp/7565/9c4411b96b467ea1b2b210b689b27ff2-man-looks-stressed-while-typing-on-laptop_l_sm.jpeg"},
-    {"title": "Anxiety Therapy For Teenagers", "link": "https://www.betterhelp.com/advice/anxiety/anxiety-therapy-for-teenagers-choosing-the-right-treatment/", "image_url": "https://dz9yg0snnohlc.cloudfront.net/advice/images/betterhelp/7436/104c67616738e47ab05186afc33cbf2c-mom-and-daughter-on-phone-girl-in-yellow-sweater_l_xsm.jpeg"},
-    {"title": "Finding The Best Local Therapist", "link": "https://www.betterhelp.com/advice/therapy/how-do-i-find-a-therapist-near-me/", "image_url": "https://dz9yg0snnohlc.cloudfront.net/advice/images/betterhelp/310/363b09cfa9f275639872fb6d3fcc7df9-couple-on-the-couch-smiling_sm.jpeg"},
-    {"title": "Effectiveness Of Art Therapy", "link": "https://www.betterhelp.com/advice/therapy/effectiveness-of-art-therapy-the-role-of-art-therapy-for-mental-health/", "image_url": "https://dz9yg0snnohlc.cloudfront.net/advice/images/betterhelp/7569/41793fbbd9726ade8de735f90be0340d-elderly-man-works-pottery_l_xsm.jpg"},
+new_blogs = [
+     {"new_title": "Do I Even Need Counseling?", "new_link": "https://www.betterhelp.com/advice/counseling/do-i-even-need-counseling-the-unique-expansiveness-of-psychotherapy/", "new_image_url": "https://dz9yg0snnohlc.cloudfront.net/advice/images/betterhelp/7563/43cc4eaab49e5d5f0bf9c8752ed9d9f7-man-stressed-headphones-white-shirt_l_sm.jpg"},
+    {"new_title": "Is It Time To Start Therapy?", "new_link": "https://www.betterhelp.com/advice/therapy/is-it-time-to-start-therapy-why-you-shouldnt-procrastinate-on-mental-health/", "new_image_url": "https://dz9yg0snnohlc.cloudfront.net/advice/images/betterhelp/7564/3888fff2403ef88711369f831050b77f-man-in-yellow-shirt-stressed-on-couch_l_sm.jpg"},
+    {"new_title": "Is It Okay To Pause Therapy?", "new_link": "https://www.betterhelp.com/advice/therapy/is-it-okay-to-pause-therapy-understanding-the-risks-and-benefits/", "new_image_url": "https://dz9yg0snnohlc.cloudfront.net/advice/images/betterhelp/7565/9c4411b96b467ea1b2b210b689b27ff2-man-looks-stressed-while-typing-on-laptop_l_sm.jpeg"},
+    {"new_title": "Anxiety Therapy For Teenagers", "new_link": "https://www.betterhelp.com/advice/anxiety/anxiety-therapy-for-teenagers-choosing-the-right-treatment/", "new_image_url": "https://dz9yg0snnohlc.cloudfront.net/advice/images/betterhelp/7436/104c67616738e47ab05186afc33cbf2c-mom-and-daughter-on-phone-girl-in-yellow-sweater_l_xsm.jpeg"},
+    {"new_title": "Finding The Best Local Therapist", "new_link": "https://www.betterhelp.com/advice/therapy/how-do-i-find-a-therapist-near-me/", "new_image_url": "https://dz9yg0snnohlc.cloudfront.net/advice/images/betterhelp/310/363b09cfa9f275639872fb6d3fcc7df9-couple-on-the-couch-smiling_sm.jpeg"},
+    {"new_title": "Effectiveness Of Art Therapy", "new_link": "https://www.betterhelp.com/advice/therapy/effectiveness-of-art-therapy-the-role-of-art-therapy-for-mental-health/", "new_image_url": "https://dz9yg0snnohlc.cloudfront.net/advice/images/betterhelp/7569/41793fbbd9726ade8de735f90be0340d-elderly-man-works-pottery_l_xsm.jpg"},
 ]
 
 class BlogPage:
-    def __init__(self, parent_frame):
-        self.parent_frame = parent_frame
+    def __init__(self, new_parent_frame):
+        self.new_parent_frame = new_parent_frame
         
         # Create a custom style for rounded rectangular labels
-        style = ttk.Style()
-        style.configure("RoundedRect.TLabel", borderwidth=1, relief="solid", padding=10, background="white",
+        new_style = ttk.Style()
+        new_style.configure("RoundedRect.TLabel", borderwidth=1, relief="solid", padding=10, background="white",
                         bordercolor="gray", borderradius=10)
 
         # Create a canvas with a vertical scrollbar
-        self.canvas = tk.Canvas(self.parent_frame, borderwidth=0)
-        vsb = tk.Scrollbar(self.parent_frame, orient="vertical", command=self.canvas.yview)
-        self.canvas.configure(yscrollcommand=vsb.set)
+        self.new_canvas = tk.Canvas(self.new_parent_frame, borderwidth=0)
+        vsb = tk.Scrollbar(self.new_parent_frame, orient="vertical", command=self.new_canvas.yview)
+        self.new_canvas.configure(yscrollcommand=vsb.set)
 
-        self.canvas.pack(side="left", fill="both", expand=True)
+        self.new_canvas.pack(side="left", fill="both", expand=True)
         vsb.pack(side="right", fill="y")
 
-        self.frame = ttk.Frame(self.canvas)
-        self.canvas.create_window((0, 0), window=self.frame, anchor="nw")
+        self.new_frame = ttk.Frame(self.new_canvas)
+        self.new_canvas.create_window((0, 0), window=self.new_frame, anchor="nw")
 
-        self.frame.bind("<Configure>", lambda event, canvas=self.canvas: canvas.configure(scrollregion=canvas.bbox("all")))
+        self.new_frame.bind("<Configure>", lambda event, canvas=self.new_canvas: canvas.configure(scrollregion=canvas.bbox("all")))
 
         # Function to handle mouse wheel scrolling
         def on_mousewheel(event):
-            self.canvas.yview_scroll(-1 * (event.delta // 120), "units")
+            self.new_canvas.yview_scroll(-1 * (event.delta // 120), "units")
 
         # Bind mouse wheel scrolling to the canvas
-        self.canvas.bind_all("<MouseWheel>", on_mousewheel)
+        self.new_canvas.bind_all("<MouseWheel>", on_mousewheel)
 
         # Calculate the width of the link boxes based on the window size
-        box_width = 470  # Half the window width
-        box_height = 300  # You can adjust this value as needed
+        new_box_width = 470  # Half the window width
+        new_box_height = 300  # You can adjust this value as needed
 
         # Create two frames, one for each column
-        self.frame1 = ttk.Frame(self.frame)
-        self.frame2 = ttk.Frame(self.frame)
-        self.frame1.grid(row=0, column=0, sticky="nsew")
-        self.frame2.grid(row=0, column=1, sticky="nsew")
+        self.new_frame1 = ttk.Frame(self.new_frame)
+        self.new_frame2 = ttk.Frame(self.new_frame)
+        self.new_frame1.grid(row=0, column=0, sticky="nsew")
+        self.new_frame2.grid(row=0, column=1, sticky="nsew")
 
         # Create labels for each blog entry with images resized to the calculated box size
-        for i, blog in enumerate(blogs):
-            title = blog["title"]
-            link = blog["link"]
-            image_url = blog["image_url"]
+        for i, new_blog in enumerate(new_blogs):
+            new_title = new_blog["new_title"]
+            new_link = new_blog["new_link"]
+            new_image_url = new_blog["new_image_url"]
 
             # Create a placeholder image
-            placeholder_img = Image.new("RGB", (box_width, box_height), "white")
-            placeholder_photo = ImageTk.PhotoImage(placeholder_img)
+            new_placeholder_img = Image.new("RGB", (new_box_width, new_box_height), "white")
+            new_placeholder_photo = ImageTk.PhotoImage(new_placeholder_img)
 
-            target_frame = self.frame1 if i % 2 == 0 else self.frame2
+            new_target_frame = self.new_frame1 if i % 2 == 0 else self.new_frame2
 
-            label = ttk.Label(
-                target_frame,
-                text=title,
-                image=placeholder_photo,
+            new_label = ttk.Label(
+                new_target_frame,
+                text=new_title,
+                image=new_placeholder_photo,
                 compound="top",
                 cursor="hand2",
                 style="RoundedRect.TLabel",
                 font=("Helvetica", 12),
             )
-            label.image = placeholder_photo
-            label.grid(row=i // 2, padx=10, pady=10, sticky="nsew")
+            new_label.image = new_placeholder_photo
+            new_label.grid(row=i // 2, padx=10, pady=10, sticky="nsew")
 
             # Load the actual image after a delay
-            self.load_image_after_delay(label, image_url, box_width, box_height)
+            self.load_image_after_delay(new_label, new_image_url, new_box_width, new_box_height)
 
-            label.bind("<Button-1>", lambda event, link=link: self.open_link(link))
+            new_label.bind("<Button-1>", lambda event, new_link=new_link: self.open_link(new_link))
 
         # Configure row and column weights for grid resizing
-        self.frame.grid_rowconfigure(0, weight=1)
-        self.frame.grid_columnconfigure(0, weight=1)
-        self.frame.grid_columnconfigure(1, weight=1)
+        self.new_frame.grid_rowconfigure(0, weight=1)
+        self.new_frame.grid_columnconfigure(0, weight=1)
+        self.new_frame.grid_columnconfigure(1, weight=1)
 
-    def open_link(self, link):
-        webbrowser.open(link)
+    def open_link(self, new_link):
+        webbrowser.open(new_link)
 
-    def load_image_after_delay(self, label, image_url, width, height):
+    def load_image_after_delay(self, new_label, new_image_url, new_width, new_height):
         # Define a function to load the image and update the label
         def load_image_and_update_label():
             try:
-                response = requests.get(image_url)
+                response = requests.get(new_image_url)
                 img = Image.open(BytesIO(response.content))
-                img = img.resize((width, height), Image.LANCZOS)  # Resize the image
-                photo = ImageTk.PhotoImage(img)
+                img = img.resize((new_width, new_height), Image.LANCZOS)  # Resize the image
+                new_photo = ImageTk.PhotoImage(img)
 
                 # Update the label with the loaded image
-                label.config(image=photo)
-                label.image = photo
+                new_label.config(image=new_photo)
+                new_label.image = new_photo
 
             except Exception as e:
                 print(f"Error loading image: {e}")
@@ -112,4 +112,4 @@ class BlogPage:
         thread.start()
 
     def destroy(self):
-        self.parent_frame.destroy()
+        self.new_parent_frame.destroy()
--- frames/exercise.py
+++ frames/exercise.py
@@ -1,31 +1,112 @@
-from datetime import datetime
+import threading
 import tkinter as tk
 from tkinter import ttk
+import webbrowser
 from PIL import Image, ImageTk
-import mysql.connector
-import helper
+import requests
+from io import BytesIO
+
+exercises = [
+     {"title": "4 Ways to Have Good Mental Health", "link": "https://www.wikihow.com/Have-Good-Mental-Health", "image_url": "https://www.wikihow.com/images/thumb/1/1a/Have-Good-Mental-Health-Step-6-Version-4.jpg/v4-460px-Have-Good-Mental-Health-Step-6-Version-4.jpg"},
+    {"title": "How to Increase Mental Strength", "link": "https://www.wikihow.com/Increase-Mental-Strength", "image_url": "https://www.wikihow.com/images/thumb/b/ba/Have-Good-Mental-Health-Step-5-Version-4.jpg/v4-460px-Have-Good-Mental-Health-Step-5-Version-4.jpg"},
+    {"title": "How to Take a Mental Health Day", "link": "https://www.wikihow.com/Take-a-Mental-Health-Day-Without-Feeling-Guilty", "image_url": "https://www.wikihow.com/images/thumb/d/db/Take-a-Mental-Health-Day-Without-Feeling-Guilty-Step-2-Version-3.jpg/v4-460px-Take-a-Mental-Health-Day-Without-Feeling-Guilty-Step-2-Version-3.jpg"},
+]
 
 class Exercise:
-    def __init__(self, parent_frame, displayname):
+    def __init__(self, parent_frame, username):
         self.parent_frame = parent_frame
-        self.displayname = displayname
-        # self.happy_button = tk.Button(self.parent_frame, text="😁", font=('Roboto', 36))
-        # self.slightly_happy_button = tk.Button(self.parent_frame, text="🙁", font=('Roboto', 36))
-        # self.sad_button = tk.Button(self.parent_frame, text="😥", font=('Roboto', 36))
-        # self.crying_button = tk.Button(self.parent_frame, text="😭", font=('Roboto', 36))
-        # self.mood_label = ttk.Label(self.parent_frame, text="Mood:", font=('Roboto', 32), anchor='center', background="#eff5f6")
-        # self.mood_value_label = ttk.Label(self.parent_frame, text="", font=('Roboto', 32), anchor='center', background='#eff5f6')
-        # self.setup_layout()
+        self.username = username
+        # Create a custom style for rounded rectangular labels
+        style = ttk.Style()
+        style.configure("RoundedRect.TLabel", borderwidth=1, relief="solid", padding=10, background="white",
+                        bordercolor="gray", borderradius=10)
 
-    # def setup_layout(self):
-    #     self.happy_button.place(x=233, y=300, anchor='center')
-    #     self.slightly_happy_button.place(x=433, y=300, anchor='center')
-    #     self.sad_button.place(x=633, y=300, anchor='center')
-    #     self.crying_button.place(x=833, y=300, anchor='center')
-    #     self.mood_label.place(x=380, y=400, anchor='center')
-    #     self.mood_value_label.place(x=600, y=400, anchor='center')
-    #     # self.save_mood_button.place(x=600, y=500, anchor='center')
-    #     # self.update_mood_value_label()
+        # Create a canvas with a vertical scrollbar
+        self.canvas = tk.Canvas(self.parent_frame, borderwidth=0)
+        vsb = tk.Scrollbar(self.parent_frame, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=vsb.set)
+
+        self.canvas.pack(side="left", fill="both", expand=True)
+        vsb.pack(side="right", fill="y")
+
+        self.frame = ttk.Frame(self.canvas)
+        self.canvas.create_window((0, 0), window=self.frame, anchor="nw")
+
+        self.frame.bind("<Configure>", lambda event, canvas=self.canvas: canvas.configure(scrollregion=canvas.bbox("all")))
+
+        # Function to handle mouse wheel scrolling
+        def on_mousewheel(event):
+            self.canvas.yview_scroll(-1 * (event.delta // 120), "units")
+
+        # Bind mouse wheel scrolling to the canvas
+        self.canvas.bind_all("<MouseWheel>", on_mousewheel)
+
+        # Calculate the width of the link boxes based on the window size
+        box_width = 470  # Half the window width
+        box_height = 300  # You can adjust this value as needed
+
+        # Create two frames, one for each column
+        self.frame1 = ttk.Frame(self.frame)
+        self.frame2 = ttk.Frame(self.frame)
+        self.frame1.grid(row=0, column=0, sticky="nsew")
+        self.frame2.grid(row=0, column=1, sticky="nsew")
+
+        # Create labels for each blog entry with images resized to the calculated box size
+        for i, blog in enumerate(exercises):
+            title = blog["title"]
+            link = blog["link"]
+            image_url = blog["image_url"]
+
+            # Create a placeholder image
+            placeholder_img = Image.new("RGB", (box_width, box_height), "white")
+            placeholder_photo = ImageTk.PhotoImage(placeholder_img)
+
+            target_frame = self.frame1 if i % 2 == 0 else self.frame2
+
+            label = ttk.Label(
+                target_frame,
+                text=title,
+                image=placeholder_photo,
+                compound="top",
+                cursor="hand2",
+                style="RoundedRect.TLabel",
+                font=("Helvetica", 12),
+            )
+            label.image = placeholder_photo
+            label.grid(row=i // 2, padx=10, pady=10, sticky="nsew")
+
+            # Load the actual image after a delay
+            self.load_image_after_delay(label, image_url, box_width, box_height)
+
+            label.bind("<Button-1>", lambda event, link=link: self.open_link(link))
+
+        # Configure row and column weights for grid resizing
+        self.frame.grid_rowconfigure(0, weight=1)
+        self.frame.grid_columnconfigure(0, weight=1)
+        self.frame.grid_columnconfigure(1, weight=1)
+
+    def open_link(self, link):
+        webbrowser.open(link)
+
+    def load_image_after_delay(self, label, image_url, width, height):
+        # Define a function to load the image and update the label
+        def load_image_and_update_label():
+            try:
+                response = requests.get(image_url)
+                img = Image.open(BytesIO(response.content))
+                img = img.resize((width, height), Image.LANCZOS)  # Resize the image
+                photo = ImageTk.PhotoImage(img)
+
+                # Update the label with the loaded image
+                label.config(image=photo)
+                label.image = photo
+
+            except Exception as e:
+                print(f"Error loading image: {e}")
+
+        # Schedule the image loading function after a delay using threading
+        thread = threading.Thread(target=load_image_and_update_label)
+        thread.start()
 
     def destroy(self):
         self.parent_frame.destroy()
